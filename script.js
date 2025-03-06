fbbfdc3a49bc6caf522a5ff4b996d1db0d22ea98
maxthebe01 <114063571+maxthebe01@users.noreply.github.com>
2025-03-06 11:07:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    alert("Contact us for a free quote: sales@miningspares.com");
+}
+
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
